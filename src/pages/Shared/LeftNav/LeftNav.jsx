6e4d4fc588bb9b42010978166cb2d68cc97a284c
Abdullah-Milon <almilonabdullah80@gmail.com>
2023-05-06 14:57:06 +0600

    set up left navbar

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
     const [categories, setCategories] = useState([]);
 
-    useEffect( () =>{
+    useEffect(() => {
         fetch('http://localhost:5000/categories')
-        .then(res => res.json())
-        .then(data => console.log(categories))
-        .catch(error => console.error(error))
-    },[])
+            .then(res => res.json())
+            // .then(data => console.log(data))
+            .then(data => setCategories(data))
+            .catch(error => console.error(error))
+    }, [])
 
     return (
         <div>
-            <h3> left nav part</h3>
+            <h3>All categories</h3>
+            <div>
+                {
+                    categories.map(category => {
+                        <p
+                            key={categories.id}
+                        >
+                        <Link>{category.name}</Link>
+                    </p>
+                    })
+                }
+            </div>
         </div>
     );
 };
