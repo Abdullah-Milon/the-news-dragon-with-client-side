b5ea3e8e67a925d7ebfc4f4e1db4e22c6ca06f2c
Abdullah-Milon <almilonabdullah80@gmail.com>
2023-05-06 08:44:58 +0600

    set up rightNav

---
+++ src/pages/Shared/QZone/QZone.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const QZone = () => {
+    return (
+        <div className='bg-secondary'>
+            <h3>Q-Zone</h3>
+        </div>
+    );
+};
+
+export default QZone;
--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,21 @@
+import { Button, ListGroup } from 'react-bootstrap';
 import React from 'react';
+import { FaGoogle, FaGithubSquare, FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 const RightNav = () => {
     return (
         <div>
-            <h3>Right nav part</h3>
+            <h3>Login With</h3>
+            <Button variant="outline-primary">  <FaGoogle /> Login With Google</Button>
+            <Button variant="outline-secondary"> <FaGithubSquare /> Login With Github</Button>
+            <div>
+                <h3>Find us on</h3>
+                <ListGroup>
+                    <ListGroup.Item> <FaFacebook/> facebook </ListGroup.Item>
+                    <ListGroup.Item> <FaTwitter/> twitter </ListGroup.Item>
+                    <ListGroup.Item> <FaInstagram/> instagram </ListGroup.Item>
+                </ListGroup>
+            </div>
         </div>
     );
 };
