bb2f3b08bb2feee5a5c4dabe1b1abeeb0f2584e0
Abdullah-Milon <almilonabdullah80@gmail.com>
2023-05-07 12:02:21 +0600

    implement the dragon news head section part

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -1,12 +1,25 @@
+import moment from 'moment';
 import React from 'react';
-import { Button, Card } from 'react-bootstrap';
+import { Button, Card, Image } from 'react-bootstrap';
+import { FaEye, FaRegBookmark, FaRegStar, FaShareAlt, FaStar } from 'react-icons/fa';
+import Rating from 'react-rating';
 import { Link } from 'react-router-dom';
 
 const NewsCard = ({ news }) => {
-    const { _id, title, details, image_url } = news;
+    const { _id, title, details, image_url, author, total_view, rating } = news;
     return (
-        <Card className="text-center mb-4">
-            <Card.Header>Featured</Card.Header>
+        <Card className=" mb-4" >
+            <Card.Header className="d-flex align-items-center" >
+                <Image style={{ height: "40px" }} src={author?.img} roundedCircle />
+                <div className='ps-2 flex-grow-1'>
+                    <p className='mb-0'>{author.name}</p>
+                    <p><small>{moment(author.published_date).format("yyyy-MM-D")} </small></p>
+                </div>
+                <div className=' '>
+                    <FaRegBookmark></FaRegBookmark>
+                    <FaShareAlt></FaShareAlt>
+                </div>
+            </Card.Header>
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
                 <Card.Img variant="top" src={image_url} />
@@ -17,9 +30,22 @@ const NewsCard = ({ news }) => {
                         </>
                     }
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
             </Card.Body>
-            <Card.Footer className="text-muted">2 days ago</Card.Footer>
+            <Card.Footer className="d-flex text-muted">
+                <div className="flex-grow-1">
+                    <Rating
+                        placeholderRating={rating?.number}
+                        readonly
+                        emptySymbol={<FaRegStar></FaRegStar>}
+                        placeholderSymbol={<FaStar className='text-danger'></FaStar>}
+                        fullSymbol={<FaStar></FaStar>}
+                    ></Rating>
+                    <span>{rating?.number}</span>
+                </div>
+                <div className="gap-2">
+                    <FaEye></FaEye>  {total_view}
+                </div>
+            </Card.Footer>
         </Card>
     );
 };
