185faee05592377c9a188c5b8916459f7573de3a
Abdullah-Milon <almilonabdullah80@gmail.com>
2023-05-07 12:53:10 +0600

    for clicking read more button then go to details of this news

--- src/pages/News/News/News.jsx
+++ src/pages/News/News/News.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+import { Link, useLoaderData } from 'react-router-dom';
+import EditorsInshits from '../EditorsInshits/EditorsInshits';
 
 const News = () => {
+    const news = useLoaderData();
+    const { _id, title, details, image_url, category_id } = news;
     return (
         <div>
-            <h2>News details comming soon</h2>
+            <Card >
+                <Card.Img variant="top" src={image_url} />
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>
+                        {details}
+                    </Card.Text>
+                    <Link to={`/category/${category_id}`}>
+                        <Button variant="danger"> <FaArrowLeft /> All news in this category</Button>
+                    </Link>
+                </Card.Body>
+                <EditorsInshits></EditorsInshits>
+            </Card>
         </div>
     );
 };
